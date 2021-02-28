7e65cc9ea2f80dbd8836449646c834bac7f3b181
Khanh Qui <vkhanhqui@gmail.com>
2021-02-28 14:02:55 +0700

    add pagination for list users

--- ShopmeProject/ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/user/UserController.java
+++ ShopmeProject/ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/user/UserController.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.util.StringUtils;
@@ -25,9 +26,28 @@ public class UserController {
 	private UserService service;
 
 	@GetMapping("/users")
-	public String listAll(Model model) {
-		List<User> listUser = service.listAll();
-		model.addAttribute("listUsers", listUser);
+	public String listFirstPage(Model model) {
+		return listByPage(1, model);
+	}
+
+	@GetMapping("/users/page/{pageNum}")
+	public String listByPage(@PathVariable(name = "pageNum") int pageNum, Model model) {
+		Page<User> page = service.listByPage(pageNum);
+		List<User> listUsers = page.getContent();
+
+		long startCount = (pageNum - 1) * UserService.USERS_PER_PAGE + 1;
+		long endCount = startCount + UserService.USERS_PER_PAGE - 1;
+		if (endCount > page.getTotalElements()) {
+			endCount = page.getTotalElements();
+		}
+
+		model.addAttribute("currentPage", pageNum);
+		model.addAttribute("totalPages", page.getTotalPages());
+		model.addAttribute("startCount", startCount);
+		model.addAttribute("endCount", endCount);
+		model.addAttribute("totalItems", page.getTotalElements());
+		model.addAttribute("listUsers", listUsers);
+
 		return "users";
 	}
 
@@ -46,22 +66,21 @@ public class UserController {
 	@PostMapping("/users/save")
 	public String saveUser(User user, RedirectAttributes redirectAttributes,
 			@RequestParam("image") MultipartFile multipartFile) throws IOException {
-		if(!multipartFile.isEmpty()) {
+		if (!multipartFile.isEmpty()) {
 			String fileName = StringUtils.cleanPath(multipartFile.getOriginalFilename());
 			user.setPhotos(fileName);
 			User savedUser = service.save(user);
-			
-			String uploadDir = "user-photos/"+savedUser.getId();
-						
+
+			String uploadDir = "user-photos/" + savedUser.getId();
+
 			FileUploadUtil.cleanDir(uploadDir);
 			FileUploadUtil.saveFile(uploadDir, fileName, multipartFile);
-			
-		
-		}else {
-			if(user.getPhotos().isEmpty()) {
+
+		} else {
+			if (user.getPhotos().isEmpty()) {
 				user.setPhotos(null);
 			}
-			service.save(user);			
+			service.save(user);
 		}
 
 		redirectAttributes.addFlashAttribute("message", "The user has been saved successfully.");
--- ShopmeProject/ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/user/UserService.java
+++ ShopmeProject/ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/user/UserService.java
@@ -6,6 +6,9 @@ import java.util.NoSuchElementException;
 import javax.transaction.Transactional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -24,9 +27,16 @@ public class UserService {
 
 	@Autowired
 	private PasswordEncoder passwordEncoder;
+	
+	public static final int USERS_PER_PAGE =4;
 
 	public List<User> listAll() {
 		return (List<User>) userRepository.findAll();
+	}
+	
+	public Page<User> listByPage(int pageNum){
+		Pageable pageable = PageRequest.of(pageNum -1, USERS_PER_PAGE);
+		return userRepository.findAll(pageable);
 	}
 
 	public List<Role> listRoles() {
